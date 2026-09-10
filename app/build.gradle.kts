4ae7db4fcde27907c9805c8fc8e3ed70b90ca56e
adescographics <adescographics2023@gmail.com>
2026-09-10 12:22:28 +0100

    Bump version to 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.statushdlite"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10502
-        versionName = "1.5.2"
+        versionCode = 10503
+        versionName = "1.5.3"
     }
 
     buildTypes {
